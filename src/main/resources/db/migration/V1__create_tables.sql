4d7067e66acea633919b4a881afe40af22575aa4
BieliaievVlad <rostik.khokhlov@gmail.com>
2024-11-12 15:30:42 +0200

    Add entities, logging, .properties, migrations

---
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -0,0 +1,51 @@
+CREATE SCHEMA IF NOT EXISTS university;
+
+CREATE TABLE IF NOT EXISTS university.groups (
+	id SERIAL PRIMARY KEY, 
+	name VARCHAR(50) NOT NULL, 
+	num_students INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS university.courses (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS university.students (
+	id SERIAL PRIMARY KEY, 
+	first_name VARCHAR(50) NOT NULL, 
+	last_name VARCHAR(50) NOT NULL, 
+	group_id INTEGER, 
+	FOREIGN KEY (group_id) REFERENCES university.groups(id) 
+);
+
+CREATE TABLE IF NOT EXISTS university.teachers (
+	id SERIAL PRIMARY KEY, 
+	first_name VARCHAR(50) NOT NULL, 
+	last_name VARCHAR(50) NOT NULL,
+	course_id INTEGER, 
+	FOREIGN KEY (course_id) REFERENCES university.courses(id)
+);
+
+CREATE TABLE IF NOT EXISTS university.students_courses (
+	student_id INTEGER, 
+	course_id INTEGER, 
+	FOREIGN KEY (student_id) REFERENCES university.students(id),
+	FOREIGN KEY (course_id) REFERENCES university.courses(id)
+);
+
+CREATE TABLE IF NOT EXISTS university.teachers_courses (
+	teacher_id INTEGER, 
+	course_id INTEGER, 
+	FOREIGN KEY (teacher_id) REFERENCES university.teachers(id), 
+	FOREIGN KEY (course_id) REFERENCES university.courses(id) 
+);
+
+CREATE TABLE IF NOT EXISTS university.shedule (
+	id SERIAL PRIMARY KEY, 
+	date_time VARCHAR(50) NOT NULL, 
+	group_id INTEGER, 
+	course_id INTEGER, 
+	FOREIGN KEY (group_id) REFERENCES university.groups(id), 
+	FOREIGN KEY (course_id) REFERENCES university.courses(id) 
+);
